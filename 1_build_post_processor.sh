ed6286f0bdd3df557da5a69c7e7b1c6f931af9b6
nx-ayoub <165767462+nx-ayoub@users.noreply.github.com>
2024-08-09 12:31:20 +0100

    Update 1_build_post_processor.sh

--- 1_build_post_processor.sh
+++ 1_build_post_processor.sh
@@ -10,6 +10,6 @@ make
 
 # Copy built postprocessor to the correct location
 sudo mkdir -p /opt/networkoptix-metavms/mediaserver/bin/plugins/nxai_plugin/nxai_manager/postprocessors
+sudo cp -f ../postprocessor-python-example/external_postprocessors.json /opt/networkoptix-metavms/mediaserver/bin/plugins/nxai_plugin/nxai_manager/postprocessors/
+sudo cp -f ./postprocessor-python-example/postprocessor-python-example /opt/networkoptix-metavms/mediaserver/bin/plugins/nxai_plugin/nxai_manager/postprocessors/
 sudo chmod -R 777 /opt/networkoptix-metavms/mediaserver/bin/plugins/nxai_plugin/nxai_manager/postprocessors/
-cp ./postprocessor-python-example/postprocessor-python-example /opt/networkoptix-metavms/mediaserver/bin/plugins/nxai_plugin/nxai_manager/postprocessors/
-cp ../postprocessor-python-example/external_postprocessors.json /opt/networkoptix-metavms/mediaserver/bin/plugins/nxai_plugin/nxai_manager/postprocessors/
